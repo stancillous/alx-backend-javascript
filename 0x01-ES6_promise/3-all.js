b28c0dfe720cb4296ff43591fd9b4ca1c32d2151
raymond <stancillousray@gmail.com>
2023-11-29 23:20:02 +0300

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,12 +4,9 @@ export default function handleProfileSignup () {
   const create_user_promise = createUser();
 
   let str = '';
-  // console.log("upload promise ", upload_photo_promise);
-  // console.log("create promise ", create_user_promise);
   upload_photo_promise.then(data => str += data.body);
   create_user_promise.then((data) => {
     str = str + ' ' + data.firstName + ' ' + data.lastName;
-    // str += data.lastName
     console.log(str);
   });
 }
