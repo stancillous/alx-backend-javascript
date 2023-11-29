b500a062fe3cb87e8c75e35a98d8c69e31fde772
raymond <stancillousray@gmail.com>
2023-11-29 15:06:43 +0300

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,15 @@
-import { uploadPhoto, createUser } from "./utils";
-export default function handleProfileSignup(){
-    const upload_photo_promise = uploadPhoto()
-    const create_user_promise = createUser()
+import { uploadPhoto, createUser } from './utils';
+export default function handleProfileSignup () {
+  const upload_photo_promise = uploadPhoto();
+  const create_user_promise = createUser();
 
-    let str = ""
-    // console.log("upload promise ", upload_photo_promise);
-    // console.log("create promise ", create_user_promise);
-    upload_photo_promise.then(data=> str += data.body)
-    create_user_promise.then((data)=>{
-        str = str+ " " +data.firstName + " " + data.lastName
-        // str += data.lastName
-        console.log(str)
-    })
-    
+  let str = '';
+  // console.log("upload promise ", upload_photo_promise);
+  // console.log("create promise ", create_user_promise);
+  upload_photo_promise.then(data => str += data.body);
+  create_user_promise.then((data) => {
+    str = str + ' ' + data.firstName + ' ' + data.lastName;
+    // str += data.lastName
+    console.log(str);
+  });
 }
