04c72e67255b035893073397e994871b7dfea1dc
raymond <stancillousray@gmail.com>
2023-11-30 17:22:32 +0300

    fix eslint errors

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
-export default function handleProfileSignup () {
-  const upload_photo_promise = uploadPhoto();
-  const create_user_promise = createUser();
+
+export default function handleProfileSignup() {
+  const uploadPromisePhoto = uploadPhoto();
+  const createUserPromise = createUser();
 
   let str = '';
-  upload_photo_promise.then(data => str += data.body);
-  create_user_promise.then((data) => {
-    str = str + ' ' + data.firstName + ' ' + data.lastName;
+  uploadPromisePhoto.then((data) => str += data.body);
+  createUserPromise.then((data) => {
+    str = `${str} ${data.firstName} ${data.lastName}`;
     console.log(str);
   });
 }
