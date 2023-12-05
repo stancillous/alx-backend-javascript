ad6aee78be2e959d7604e24e97ca1d1c220d6b29
raymond <stancillousray@gmail.com>
2023-12-05 12:57:35 +0300

    avoid changing value of function parameter. create copy instead

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,14 +2,15 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   const studentsInCity = students.filter((student) => student.location === city);
 
   const newStudentsGrade = studentsInCity.map((student) => {
+    let student_copy = student
     for (const grade of newGrades) {
-      if (grade.studentId === student.id) {
-        student.grade = grade.grade;
-        return student;
+      if (grade.studentId === student_copy.id) {
+        student_copy.grade = grade.grade;
+        return student_copy;
       }
     }
-    student.grade = 'N/A';
-    return student;
+    student_copy.grade = 'N/A';
+    return student_copy;
   });
   return newStudentsGrade;
 }
