4fb9e1f7f7f8a9c6a6e11433723956794bde5faf
raymond <stancillousray@gmail.com>
2023-12-16 20:56:40 +0300

    solutions for task5

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,18 @@
+interface MajorCredits {
+    credits: number,
+    major: true
+}
+
+interface MinorCredits {
+    credits: number,
+    major: false
+}
+
+// will finish up after task 4
+function sumMajorCredits(subject1, subject2) {
+    
+}
+
+function sumMinorCredits(subject1, subject2) {
+
+}
